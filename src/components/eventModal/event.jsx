923a6ce7ca6729631dee13c689fdbf60db63ec8f
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-09-05 16:26:11 +0200

    event.jsx
    need to do eventDateStart. it should show default value selected date in form

--- src/components/eventModal/event.jsx
+++ src/components/eventModal/event.jsx
@@ -1,74 +1,89 @@
-import { useState } from 'react'
+import React, { useContext } from 'react'
 import {BiTimeFive, BiLocationPlus} from 'react-icons/bi'
+import { useEventForm } from '../../utils/eventFormUtil';
+import GlobalContext from '../../context/globalContext';
 
 
 export default function EventForm() {
-    const [eventName, setEventName] = useState('');
-    const [eventDateStart, setEventDateStart] = useState('');
-    const [eventDateEnd, setEventDateEnd]= useState('');
-    const [eventTimeStart, setEventTimeStart]= useState('');
-    const [eventTimeEnd, setEventTimeEnd]= useState('');
+    const [formData, updateField]= useEventForm();
+    const selectedDay = useContext(GlobalContext)
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        const { eventName, eventDateStart, eventDateEnd, eventTimeStart, location } = formData;
+        // uložení dat do local storage
+        const eventData = {
+            eventName,
+            eventDateStart,
+            eventDateEnd,
+            eventTimeStart,
+            location,
+          };
+        const existingData = localStorage.getItem('eventData');
+        const parsedData = existingData ? JSON.parse(existingData) : [];
+        parsedData.push(eventData);
+        localStorage.setItem('eventData', JSON.stringify(parsedData));
+    }
    
-    return (
-        <form>
-            <div className='eventBody'>
-            
-                <div className='title'>
-                    <input 
-                        type='text' 
-                        name='eventName' 
-                        placeholder='Event name' 
-                        value={eventName} 
-                        required
-                        onChange={(e)=> setEventName(e.target.value)} 
-                    />
+   return (
+    <form>
+      <div className='eventBody'>
+        <div className='title'>
+          <input 
+            type='text' 
+            name='eventName' 
+            placeholder='Event name' 
+            value={formData.eventName} 
+            required
+            onChange={(e) => updateField('updateEventName', e.target.value)} 
+          />
+        </div>
+        <div className='dateDiv'>
+                <BiTimeFive/>
+            <div>
+                <input
+                    type='date'
+                    name='eventStartDate'
+                    value={formData.eventDateStart}
+                    placeholder='start of event'
+                    required
+                    onChange={(e) => updateField('updateEventDateStart', e.target.value)}
+                />
+                <input type='date' 
+                name='eventEndDate' 
+                value={formData.eventDateEnd}
+                required
+                onChange={(e) => updateField('updateEventDateEnd', e.target.value)}
+                />
+                <div>
+                <input type='time'
+                    name='eventTimeStart'
+                    value={formData.eventTimeStart}
+                    required
+                    onChange={(e) => updateField('updateEventTimeStart', e.target.value)}
+                />
                 </div>
-                <div className='dateDiv'>
-                    <BiTimeFive/>
-                    <div>
-                        <input type='date' 
-                            name='eventStartDate' 
-                            value={eventDateStart} 
-                            placeholder='start of event'
-                            required
-                            onChange={(e)=>setEventDateStart(e.target.value)}    
-                        />
-                        <input type='date' 
-                            name='eventEndDate' 
-                            value={eventDateEnd}
-                            required
-                            onChange={(e)=>setEventDateEnd(e.target.value)}
-                        />
-                        <div>
-                            <input type='time'
-                                name='eventTimeStart'
-                                value={eventTimeStart}
-                                required
-                                onChange={(e)=>setEventTimeStart(e.target.value)}
-                            />
-                            <input type='time'
-                                name='eventTimeEnd'
-                                value={eventTimeEnd}
-                                required
-                                onChange={(e)=>setEventTimeEnd(e.target.value)}
-                            />
-                        </div>
-
-                    </div>
-                </div>
-                <div className='location'>
-                    <BiLocationPlus/>
-                    <input type='text'/>
-                </div>
-                <footer>
-                    <button 
-                        type='submit'
-
-                    >
-                        Save
-                    </button>
-                </footer>
-            </div>
-        </form>
-    )
+          </div>
+        </div>
+        <div className='location'>
+          <BiLocationPlus/>
+          <input type='text'
+            onChange={(e) => updateField('updateLocation', e.target.value)}
+            name='location'
+            value={formData.location}
+            required
+          />
+        </div>
+        <footer>
+          <button 
+            type='submit'
+            onClick={handleSubmit}
+          >
+            Save
+          </button>
+        </footer>
+      </div>
+    </form>
+  );
 }
